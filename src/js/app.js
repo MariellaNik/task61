b506d7d941d96ce688b51d17d864b132f77642bc
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-01-29 15:37:32 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   // Select the price element with the .hot class
 const hotPrice = document.querySelectorAll('.price');
-
-// Create a new text node with the fire emoji
-const fireEmoji = document.createTextNode('🔥');
-
-// Append the fire emoji to the hot price element
-//hotPrice.appendChild(fireEmoji);
+const fireEmoji = '🔥';
+hotPrice.textContent = hotPrice.textContent + fireEmoji;
 
 });
