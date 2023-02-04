7aa3796e9f054b832a49f809e84b3b04d960cdab
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-04 10:39:22 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-  // Select the price element with the .hot class
-  if(class==".hot class"){
-    console.log(${price}+ " "+'🔥');
-}
-//let hotPrice = document.querySelectorAll('.price');
-//hotPrice.textContent = hotPrice.textContent + '🔥';
-
 });
+ const prices = document.querySelectorAll(".price");
+
+prices.forEach((price) => {
+  if (price.classList.contains("hot")) {
+    const fireEmoji = document.createTextNode("🔥");
+    price.appendChild(fireEmoji);
+  }
+});
+
